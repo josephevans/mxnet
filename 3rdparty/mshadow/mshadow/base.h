cd563cc03fcd40787a549798ebe08fec6f91efb9
Joe Evans <joeev@amazon.com>
2020-03-11 15:02:08 -0700

    Use correct definition for determining c++11 compliance in destructor.

--- 3rdparty/mshadow/mshadow/base.h
+++ 3rdparty/mshadow/mshadow/base.h
@@ -231,7 +231,7 @@ extern "C" {
 #define MSHADOW_USE_GLOG DMLC_USE_GLOG
 #endif  // MSHADOW_USE_GLOG
 
-#if DMLC_USE_CXX11
+#if MSHADOW_IN_CXX11
 #define MSHADOW_THROW_EXCEPTION noexcept(false)
 #define MSHADOW_NO_EXCEPTION  noexcept(true)
 #else
